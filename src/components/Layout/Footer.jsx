334dd6d028b41700c3c02d0b13c0ecb9db906d6d
Kim Jin Young <116090051+jyk2022@users.noreply.github.com>
2023-03-23 21:46:22 +0900

    링크 교체

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <>
       <Footers>
         <h1> 저에 대해 더 알고 싶으시면 아래 배너를 클릭해주세요!</h1>
-        <a href="https://jykthink.notion.site/718813a9ba74444f8618148ad21079bc">
+        <a href="https://jykthink.notion.site/JYK-667430a303b649059d37443f0faf88cb">
           <img
             src={`${process.env.PUBLIC_URL}/images/notion.jpg`}
             alt="노트폴리오 배너"
